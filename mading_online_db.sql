28ef003479999918d7ac0fc7c03fa5105dfb3d4b
Achmad Tantowi <achmad.tantowi@edutren.com>
2018-07-05 22:21:46 +0700

    update

--- mading_online_db.sql
+++ mading_online_db.sql
@@ -11,11 +11,34 @@
  Target Server Version : 100132
  File Encoding         : 65001
 
- Date: 24/06/2018 17:33:18
+ Date: 05/07/2018 22:19:51
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for contents
+-- ----------------------------
+DROP TABLE IF EXISTS `contents`;
+CREATE TABLE `contents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of contents
+-- ----------------------------
+BEGIN;
+INSERT INTO `contents` VALUES (1, 'tes', '<p>tes</p>', '2018-06-27 03:48:09', '2018-06-27 03:48:09');
+INSERT INTO `contents` VALUES (2, 'oke', '<p><strong>sdsd</strong></p>\r\n\r\n<p><strong>dsdsdsd</strong></p>\r\n\r\n<p><strong><s>xcxc</s></strong></p>', '2018-06-27 03:48:43', '2018-06-27 03:48:43');
+INSERT INTO `contents` VALUES (3, 'rtrt', '<p>ssdsd</p>\r\n\r\n<p>sdsd</p>\r\n\r\n<p>wewe</p>', '2018-06-27 03:49:04', '2018-06-27 03:49:04');
+INSERT INTO `contents` VALUES (4, 'lkllk', '<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"width:500px\">\r\n	<tbody>\r\n		<tr>\r\n			<td>sd</td>\r\n			<td>df</td>\r\n		</tr>\r\n		<tr>\r\n			<td>xcxc</td>\r\n			<td>we</td>\r\n		</tr>\r\n		<tr>\r\n			<td>c</td>\r\n			<td>as</td>\r\n		</tr>\r\n	</tbody>\r\n</table>\r\n\r\n<p>&nbsp;</p>', '2018-06-27 03:56:57', '2018-06-27 03:56:57');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for migrations
@@ -88,7 +111,7 @@ CREATE TABLE `users` (
 -- Records of users
 -- ----------------------------
 BEGIN;
-INSERT INTO `users` VALUES (1, 'Achmad Tantowi', NULL, 'http://pbs.twimg.com/profile_images/869783174401753089/0x3ApbPQ_normal.jpg', '65d90fc6d307590b14e9e1800d4e8eab', 'w0SxhCelF6c805R7LQd0oO0F4DjjkNeGZP9wx8AZVyzN4WcjRBs0E23K2DAW', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
+INSERT INTO `users` VALUES (1, 'Achmad Tantowi', NULL, 'http://pbs.twimg.com/profile_images/869783174401753089/0x3ApbPQ_normal.jpg', '65d90fc6d307590b14e9e1800d4e8eab', 'SoMsJyLyb7qv1m3i6WZWwbzi5JSC7i182TWkPvucZWHPt3d64Rzvcrc3mkOK', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
 INSERT INTO `users` VALUES (2, 'Joko', 'joko@mail.com', NULL, '$2y$10$K/AtjmzYv3SnTsWxMFf1iuRmp7Fda9q7vHEaJcp9EN66ocseq/CyC', 'GcZCHdwJylpwaCBQElKfgrDjVaw4A9lVFHbark3j6IIEpV105sdNUpNa5vdE', '2018-06-18 09:38:05', '2018-06-18 09:38:05');
 COMMIT;
 
