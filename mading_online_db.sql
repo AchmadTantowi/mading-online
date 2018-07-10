c848a7b798471e31935f2bb546489d8b8b0bbc72
Achmad Tantowi <achmad.tantowi@edutren.com>
2018-07-10 07:08:57 +0700

    update DB

--- mading_online_db.sql
+++ mading_online_db.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100132
  File Encoding         : 65001
 
- Date: 08/07/2018 20:07:48
+ Date: 10/07/2018 07:08:06
 */
 
 SET NAMES utf8mb4;
@@ -24,21 +24,23 @@ DROP TABLE IF EXISTS `contents`;
 CREATE TABLE `contents` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `image` text,
+  `content` text,
+  `active` int(1) NOT NULL DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of contents
 -- ----------------------------
 BEGIN;
-INSERT INTO `contents` VALUES (1, 'tes', '<p>tes</p>', '2018-06-27 03:48:09', '2018-06-27 03:48:09');
-INSERT INTO `contents` VALUES (2, 'oke', '<p><strong>sdsd</strong></p>\r\n\r\n<p><strong>dsdsdsd</strong></p>\r\n\r\n<p><strong><s>xcxc</s></strong></p>', '2018-06-27 03:48:43', '2018-06-27 03:48:43');
-INSERT INTO `contents` VALUES (3, 'rtrt', '<p>ssdsd</p>\r\n\r\n<p>sdsd</p>\r\n\r\n<p>wewe</p>', '2018-06-27 03:49:04', '2018-06-27 03:49:04');
-INSERT INTO `contents` VALUES (4, 'lkllk', '<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"width:500px\">\r\n	<tbody>\r\n		<tr>\r\n			<td>sd</td>\r\n			<td>df</td>\r\n		</tr>\r\n		<tr>\r\n			<td>xcxc</td>\r\n			<td>we</td>\r\n		</tr>\r\n		<tr>\r\n			<td>c</td>\r\n			<td>as</td>\r\n		</tr>\r\n	</tbody>\r\n</table>\r\n\r\n<p>&nbsp;</p>', '2018-06-27 03:56:57', '2018-06-27 03:56:57');
-INSERT INTO `contents` VALUES (6, 'okeoke', '<p>sdsdsds</p>', '2018-07-07 06:27:40', '2018-07-07 06:27:40');
+INSERT INTO `contents` VALUES (7, 'Penyejuk jiwa', 'Quote', NULL, '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>', 1, '2018-07-09 14:47:49', '2018-07-09 14:47:49');
+INSERT INTO `contents` VALUES (9, 'Selamat Datang di Website Universitas Pamulang', 'Information', NULL, '<p>Kami Menyambut baik dengan adanya Website Universitas Pamulang yang baru, dengan harapan dipublikasinya website ini kampus berharap : Peningkatan layanan pendidikan kepada mahasiswa/i, orangtua, dan masyarakat pada umumnya semakin meningkat. Sebaliknya orangtua dapat mengakses informasi tentang kegiatan akademik dan non akademik putra &ndash; puterinya di kampus ini.</p>', 1, '2018-07-09 14:53:02', '2018-07-09 14:53:02');
+INSERT INTO `contents` VALUES (10, 'Pengukuhan guru besar', 'Banner', 'web-banner-guru-besar.jpg', NULL, 1, '2018-07-09 23:33:34', '2018-07-09 23:33:34');
+INSERT INTO `contents` VALUES (11, 'Kluster utama', 'Banner', 'web-banner-prestasi-penelitian.jpg', NULL, 1, '2018-07-09 23:48:03', '2018-07-09 23:48:03');
 COMMIT;
 
 -- ----------------------------
@@ -98,18 +100,20 @@ CREATE TABLE `password_resets` (
 -- ----------------------------
 DROP TABLE IF EXISTS `social_twitter_accounts`;
 CREATE TABLE `social_twitter_accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `provider_user_id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `provider` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of social_twitter_accounts
 -- ----------------------------
 BEGIN;
-INSERT INTO `social_twitter_accounts` VALUES (1, '869776780546023424', 'twitter', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
+INSERT INTO `social_twitter_accounts` VALUES (1, 1, '869776780546023424', 'twitter', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
 COMMIT;
 
 -- ----------------------------
@@ -127,14 +131,14 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
 BEGIN;
-INSERT INTO `users` VALUES (1, 'Achmad Tantowi', NULL, 'http://pbs.twimg.com/profile_images/869783174401753089/0x3ApbPQ_normal.jpg', '65d90fc6d307590b14e9e1800d4e8eab', 'SoMsJyLyb7qv1m3i6WZWwbzi5JSC7i182TWkPvucZWHPt3d64Rzvcrc3mkOK', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
-INSERT INTO `users` VALUES (2, 'Joko', 'joko@mail.com', NULL, '$2y$10$K/AtjmzYv3SnTsWxMFf1iuRmp7Fda9q7vHEaJcp9EN66ocseq/CyC', 'QYQNi6WY8IWdgMenwr0iMW9ltIVSNjwS2JPFTuX5Tcc6EX2kCzsOTIddL2A0', '2018-06-18 09:38:05', '2018-06-18 09:38:05');
+INSERT INTO `users` VALUES (1, 'Achmad Tantowi', NULL, 'http://pbs.twimg.com/profile_images/869783174401753089/0x3ApbPQ_normal.jpg', '65d90fc6d307590b14e9e1800d4e8eab', 'Er16VLE6CGbNCC2ou2KSOaenVl8xdgoGj98huoJArzReWVUfTVAzA2eGRnZa', '2018-06-18 09:30:31', '2018-06-18 09:30:31');
+INSERT INTO `users` VALUES (2, 'Joko', 'joko@mail.com', NULL, '$2y$10$K/AtjmzYv3SnTsWxMFf1iuRmp7Fda9q7vHEaJcp9EN66ocseq/CyC', 'neDQMHaGwMP3RkbpDQZdLPbjxrqhswYQO1PKQoHrcOkYbJq4SxkY9bUQon3d', '2018-06-18 09:38:05', '2018-06-18 09:38:05');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
