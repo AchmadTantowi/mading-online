9481e8a8c37880415feabb22df8665f49e9f2e6d
AgieAja <tiar@edutren.com>
2018-07-24 16:48:51 +0700

    update token key twiiter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row" style="margin-left:-380px;margin-right:-380px;">
-        <div class="col-md-6">
+        <div class="col-md-5">
             <div class="box">
                 <!-- <div class="box-header with-border">
                     <h3 class="box-title">Monthly Recap Report</h3>
@@ -63,11 +63,11 @@
                         <span class="fa fa-angle-right"></span>
                         </a> -->
                     <!-- </div> -->
-                </div><br><br><br>
+                </div>
             </div>
         </div>
 
-        <section class="col-md-6" style="padding-right:50px;">
+        <section class="col-md-5" style="padding-right:50px;">
             <div class="box box-solid">
             <div class="box-header with-border">
             
